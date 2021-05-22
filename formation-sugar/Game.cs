1df3f055947879217371bd9e7decfe114df23ae9
Arcibashev <arcibashev71@yandex.ru>
2021-05-22 21:11:50 +0500

    minor refactoring in Game

--- formation-sugar/Game.cs
+++ formation-sugar/Game.cs
@@ -12,17 +12,17 @@ namespace formation_sugar
     public sealed partial class Game : Form
     {
         private GameMap map;
-        private Dictionary<ICreature, Dictionary<(MovementCondition, Direction), Animation>> animationsForCreatures;
-        private bool wIsPressed;
-        private bool aIsPressed;
-        private bool dIsPressed;
-        private bool spaceIsPressed;
-        private Label playerHealthPoints;
-        private Label score;
         private Timer timerForCreaturesActions;
         private Timer timerForCreaturesAnimations;
         private Timer timerForHearthAnimation;
         private List<Timer> timers;
+        private Dictionary<ICreature, Dictionary<(MovementCondition, Direction), Animation>> animationsForCreatures;
+        private Label playerHealthPoints;
+        private Label score;
+        private bool wIsPressed;
+        private bool aIsPressed;
+        private bool dIsPressed;
+        private bool spaceIsPressed;
 
         public Game()
         {
@@ -64,9 +64,7 @@ namespace formation_sugar
         protected override void OnKeyUp(KeyEventArgs e)
         {
             if (map.Player.IsDead())
-            {
                 return;
-            }
 
             switch (e.KeyCode)
             {
@@ -122,19 +120,23 @@ namespace formation_sugar
                 CreatureLocationAndConditionsUpdater.UpdateLocationAndCondition(map);
                 map.RemoveCreaturesFromMapIfTheyAreDead();
                 map.MakeEnemiesAttackingOrRunning();
-                playerHealthPoints.Text = map.Player.Health.ToString();
-                score.Text = Text = @"Score: " + map.Score;
+                UpdateInfoAboutGame();
             };
+        }
+
+        private void UpdateInfoAboutGame()
+        {
+            playerHealthPoints.Text = map.Player.Health.ToString();
+            score.Text = Text = @"Score: " + map.Score;
         }
 
         private void ResetTimerForCreaturesAnimations()
         {
-            timerForCreaturesAnimations = new Timer {Interval = 100, Enabled = true};
+            timerForCreaturesAnimations = new Timer {Interval = 85, Enabled = true};
             timerForCreaturesAnimations.Tick += delegate
             {
                 foreach (var creature in map.ListOfCreatures)
                     animationsForCreatures[creature][(creature.MovementCondition, creature.Direction)].MoveNextSprite();
-
                 Invalidate();
             };
         }
@@ -217,7 +219,6 @@ namespace formation_sugar
                 MapCreator.ResetLevel();
                 map = new GameMap();
             }
-
             else if (GameWon() || nextLevel)
                 map.LoadNextMap(map.Score);
             else
